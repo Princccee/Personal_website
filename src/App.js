8624d3f9f695fd4cf9126f2da248a040ab592d30
Prince Kumar <119061630+Princccee@users.noreply.github.com>
2025-09-02 15:49:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const Home = () => (
             I’m a final-year B.Tech student in Data Science and AI at IIIT Dharwad, passionate about developing intelligent and scalable software solutions. My experience spans AI/ML, backend systems, and full-stack applications, where I’ve built projects ranging from real-time delivery platforms to NLP-powered automation tools. I enjoy solving complex problems and creating technology that makes a real-world impact.
           </p>
           
-          <Connect linkedin="https://www.linkedin.com/in/prince-kumar-127903288/" github="https://github.com/Princccee" contact="9123174099"
+          <Connect linkedin="https://www.linkedin.com/in/prince-kumar-127903288/" github="https://github.com/Princccee" gmail="22bds046@iiitdwd.ac.in" contact="9123174099"
           resume="https://drive.google.com/file/d/1bF8MxWWgRwvl1BaN9tdXDcZb_GGsKdvj/view?usp=sharing"/>
           
         </div>
